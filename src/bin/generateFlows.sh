517fdfa4be67b08d85d631c24eb8f819ab60c517
Gabriel Weaver <gweaver@illinois.edu>
2019-12-20 13:32:05 -0600

    OK

--- src/bin/generateFlows.sh
+++ src/bin/generateFlows.sh
@@ -3,7 +3,7 @@ echo "Data Intersection"
 python3 src/bin/dataIntersection.py `pwd`/data/test-scenarios/onf2 data/test-scenarios/onf2/data/PEV-FY2018/counts.txt
 echo "Vessel Schedule"
 python3 src/bin/generateVesselSchedule.py `pwd`/data/test-scenarios/onf2
-python3 src/bin/filterSchedule.py `pwd`/data/test-scenarios/onf2 
+python3 src/bin/filterSchedule.py 0 10080 `pwd`/data/test-scenarios/onf2 
 echo "Shipments"
 python3 src/bin/generateVesselShipments.py `pwd`/data/test-scenarios/onf2
 python3 src/bin/filterShipments.py `pwd`/data/test-scenarios/onf2 
